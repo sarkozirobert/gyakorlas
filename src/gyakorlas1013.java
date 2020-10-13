63de02c3cfc094d428e223f5ca265a4ba165dc15
Sárközi Róbert <sark.robert@gmail.com>
2020-10-13 14:18:41 +0200

    tömbDeklarálás

--- src/gyakorlas1013.java
+++ src/gyakorlas1013.java
@@ -1,5 +1,10 @@
 public class gyakorlas1013 {
     public static void main(String[] args) {
-
+        //Deklarálj a main metódusodban egy 20 hosszú tömböt, amit tölts fel 1 és 100 közötti
+        //random számokkal!
+        int[] numbers = new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 100);
+        }
     }
 }
